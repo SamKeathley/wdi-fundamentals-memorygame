f3adffe788d963f1c8875be45de16ae3d51e0f27
Sam Keathley <samncollette@gmail.com>
2017-02-25 00:30:26 -0600

    added cards in js

--- starter-code/main.js
+++ starter-code/main.js
@@ -5,10 +5,19 @@ var cardTwo = "king";
 var cardThree = "queen";
 var cardFour = "king";
 
-if (cardOne === cardThree) {
-	alert("You found a match!");
-} else if (cardTwo === cardFour) {
-	alert("You found a match!");
-} else if (cardOne === cardTwo && cardThree === cardFour) {
-	alert("Sorry, try again.");
-} 
+//if (cardOne === cardThree) {
+//	alert("You found a match!");
+//} else if (cardTwo === cardFour) {
+//	alert("You found a match!");
+//} else if (cardOne === cardTwo && cardThree === cardFour) {
+//	alert("Sorry, try again.");
+//} 
+function createCards(){
+	var board = document.getElementById('game-board');
+	for (var i = 0; i < 4; i++) {
+		var card = document.createElement('div');
+		card.className = 'card';
+		board.appendChild(card);
+	}
+}
+createCards();
