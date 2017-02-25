221f377c9a18cc000b7f20948dbb7ed5a647abbb
Sam Keathley <samncollette@gmail.com>
2017-02-24 22:01:54 -0600

    created alerts

--- starter-code/main.js
+++ starter-code/main.js
@@ -5,3 +5,10 @@ var cardTwo = "king";
 var cardThree = "queen";
 var cardFour = "king";
 
+if (cardOne === cardThree) {
+	alert("You found a match!");
+} else if (cardTwo === cardFour) {
+	alert("You found a match!");
+} else if (cardOne === cardTwo && cardThree === cardFour) {
+	alert("Sorry, try again.");
+} 
